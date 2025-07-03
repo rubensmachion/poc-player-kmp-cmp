e225ca240f3f2f3bc2330ffc3978fea627dd4628
Rubens <r1b2ns@duck.com>
2025-07-03 11:42:26 -0300

    delete files

--- composeApp/src/commonMain/kotlin/com/poc/videoplayerkmp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.poc.videoplayerkmp
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/poc/videoplayerkmp/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.poc.videoplayerkmp
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
